10db7396b3821b2406b25ff3904a38aa3fa53a56
Lucas de Castro <119894552+castrolucas05@users.noreply.github.com>
2023-05-28 14:03:46 -0300

    att

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// Seleção de elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+// Funções
+const saveTodo = (text) =>{ // aqui vamos criar todo o html do To Do List
+
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
+
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    todo.appendChild(doneBtn)
+    
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    todo.appendChild(editBtn)
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("remove-todo");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    todo.appendChild(deleteBtn);
+
+    todoList.appendChild(todo);
+    todoInput.value = "";
+    todoInput.focus();
+}
+
+// Eventos "e"
+todoForm.addEventListener("submit", (e) => { // envia o formulario sem recarregar a pagina, e não enviar para nenhum banco back end
+    e.preventDefault();
+
+    const inputValue = todoInput.value;
+    if(inputValue){
+        saveTodo(inputValue)
+    }
+
+});
